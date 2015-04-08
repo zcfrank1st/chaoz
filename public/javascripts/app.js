4885ca3a48cacb19c0f5f1b78b5d369027bcd563
zcfrank1st <zcfrank1st@sina.com>
2015-04-08 09:43:08 +0800

    增加各个view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,14 @@
 /**
  * Created by zcfrank1st on 4/7/15.
  */
-var chaoz = angular.module('chaoz',[]);
+var chaoz = angular.module('chaoz',['ui.router']);
+
+chaoz.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/search");
+
+    $stateProvider
+        .state('search', {
+            url: "/search",
+            templateUrl: "template/search.html"
+        })
+});
