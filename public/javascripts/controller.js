49e003a5d56b1f9b64c3ae0b8c9883d12a2ff685
zcfrank1st <zcfrank1st@sina.com>
2015-04-10 21:56:01 +0800

    分享页面

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -13,16 +13,26 @@ chaoz.controller('search', function($scope){
     $scope.default = true;
     $scope.find = function () {
         $scope.default = false;
+        $scope.result = true;
         $scope.pagination = true;
     };
 
     $scope.latest10 = function () {
         $scope.default = false;
+        $scope.result = true;
         $scope.pagination = false;
     };
 
     $scope.return = function () {
         $scope.default = true;
+        $scope.result = false;
+        $scope.shareList = false;
+    };
+
+    $scope.share = function () {
+        $scope.default = false;
+        $scope.result = false;
+        $scope.shareList = true;
     };
 
     $scope.items = [
@@ -30,18 +40,24 @@ chaoz.controller('search', function($scope){
             type: "ebook",
             name: "book1",
             description: "book",
+            link: "",
+            comment: "",
             timestamp: "2015-04-08"
         },
         {
             type: "torrent",
             name: "everyday",
             description: "bt file",
+            link: "",
+            comment: "",
             timestamp: ""
         },
         {
             type: "video",
             name: "movie",
             description: "video",
+            link: "",
+            comment: "",
             timestamp: ""
         }
     ]
