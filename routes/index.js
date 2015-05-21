a13353ca5d492af2a4358f4585205a2f4a50a5de
zcfrank1st <zcfrank1st@sina.com>
2015-05-21 22:05:44 +0800

    小修改

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'CHaos' });
+  res.render('index', { title: '混沌区' });
 });
 
 module.exports = router;
