2b832e0431d15e351e759e70fe3c9e8fd46948a9
zcfrank1st <zcfrank1st@sina.com>
2015-05-22 09:31:24 +0800

    api修改

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var action = require('./routes/action');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/action', action);
+//app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/action.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/insert', function(req, res) {
+
+});
+
+router.post('/update', function(req, res) {
+
+});
+
+router.get('/query', function(req, res) {
+
+});
+
+module.exports = router;
